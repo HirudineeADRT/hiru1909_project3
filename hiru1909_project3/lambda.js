532574241ffa4f7a5d6109be0caebaf882c5aefa
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-09-19 16:33:38 +0530

    add ddb resoure with global changes

--- hiru1909_project3/lambda.js
+++ hiru1909_project3/lambda.js
@@ -1,4 +1,16 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = function (event, context, callback) {
+    ddb.scan({
+        TableName: 'test'
+    }).promise()
+        .then((data) => {
+            //your logic goes here
+        })
+        .catch((err) => {
+            //handle error
+        });
 
     callback(null, { "message": "Successfully executeqqd" });
 }
